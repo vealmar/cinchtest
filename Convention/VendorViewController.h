a435e6c00c5c49a48fa925a817c7e66011082320
Kerry Sanders <ksanders@doozer.com>
2013-01-12 22:50:50 -0600

    Added new view to handle selection of vendors.

---
+++ Convention/VendorViewController.h
@@ -0,0 +1,13 @@
+//
+//  VendorViewController.h
+//  Convention
+//
+//  Created by Kerry Sanders on 1/12/13.
+//  Copyright (c) 2013 MotionMobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VendorViewController : UITableViewController
+
+@end
