052f39529105c64096b807e3ab416f4900973cfd
Kerry Sanders <ksanders@doozer.com>
2013-01-13 21:31:26 -0600

    Added Added new view to handle selection of bulletins.

---
+++ Convention/BulletinViewController.h
@@ -0,0 +1,13 @@
+//
+//  BulletinViewController.h
+//  Convention
+//
+//  Created by Kerry Sanders on 1/13/13.
+//  Copyright (c) 2013 MotionMobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BulletinViewController : UITableViewController
+
+@end
---
+++ Convention/VendorViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  VendorViewDelegate.h
+//  Convention
+//
+//  Created by Kerry Sanders on 1/13/13.
+//  Copyright (c) 2013 MotionMobs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol VendorViewDelegate <NSObject>
+
+@end
