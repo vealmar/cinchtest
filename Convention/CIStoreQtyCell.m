1d29708386139bac99a4a47dc92b5e8b69d7ef96
Kerry Sanders <ksanders@doozer.com>
2013-01-16 23:08:07 -0600

    Text field was not resigning first responder which caused the keyboard to get stuck open.

--- Convention/CIStoreQtyCell.m
+++ Convention/CIStoreQtyCell.m
@@ -32,12 +32,12 @@
     // Configure the view for the selected state
 }
 
--(BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [textField resignFirstResponder];
-//    if (self.delegate)
-//        [self.delegate selectNextRow:self.tag];
-    return YES;
-}
+//-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+//    [textField resignFirstResponder];
+////    if (self.delegate)
+////        [self.delegate selectNextRow:self.tag];
+//    return YES;
+//}
 
 -(BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
     originalText = [NSString stringWithString:textField.text];
@@ -45,18 +45,18 @@
 }
 
 -(void)textFieldDidEndEditing:(UITextField *)textField {
-    
+    [textField resignFirstResponder];
     // Revert the text to original value
     // if nothing is in the field.
     if ([textField.text isEmpty])
         textField.text = [NSString stringWithString:originalText];
     
-    UITableView *tableView = (UITableView *)self.superview;
-    int nextRow = self.tag + 1;
-    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:nextRow inSection:0];
-    CIStoreQtyCell *nextCell = (CIStoreQtyCell *)[tableView cellForRowAtIndexPath:indexPath];
-    if (nextCell)
-        [nextCell.Qty becomeFirstResponder];
+//    UITableView *tableView = (UITableView *)self.superview;
+//    int nextRow = self.tag + 1;
+//    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:nextRow inSection:0];
+//    CIStoreQtyCell *nextCell = (CIStoreQtyCell *)[tableView cellForRowAtIndexPath:indexPath];
+//    if (nextCell)
+//        [nextCell.Qty becomeFirstResponder];
 //    if (cellPath.row + 1 > [tableView numberOfRowsInSection:0]) {
 //        NSIndexPath *nextCellPath = [NSIndexPath indexPathForRow:cellPath.row + 1 inSection:cellPath.section];
 //        CIStoreQtyCell *nextCell = (CIStoreQtyCell *)[tableView cellForRowAtIndexPath:nextCellPath];
