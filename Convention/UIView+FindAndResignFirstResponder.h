cf6f7d956583619672280d2bd3a848e5b0aff211
Kerry Sanders <ksanders@doozer.com>
2013-05-09 02:44:30 -0500

    no message

---
+++ Convention/UIView+FindAndResignFirstResponder.h
@@ -0,0 +1,13 @@
+//
+//  UIView+FindAndResignFirstResponder.h
+//  Convention
+//
+//  Created by Kerry Sanders on 5/9/13.
+//  Copyright (c) 2013 MotionMobs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (FindAndResignFirstResponder)
+
+@end
---
+++ Convention/UIView+FindAndResignFirstResponder.m
@@ -0,0 +1,13 @@
+//
+//  UIView+FindAndResignFirstResponder.m
+//  Convention
+//
+//  Created by Kerry Sanders on 5/9/13.
+//  Copyright (c) 2013 MotionMobs. All rights reserved.
+//
+
+#import "UIView+FindAndResignFirstResponder.h"
+
+@implementation UIView (FindAndResignFirstResponder)
+
+@end
