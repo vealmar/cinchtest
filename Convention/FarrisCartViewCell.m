d5eef06782a53dbc9bf935d49f117a889104f5b7
Swapna Gupta <swapna.sony@gmail.com>
2014-01-12 18:28:00 -0600

    Displayed Min column for Farris.

--- Convention/FarrisCartViewCell.m
+++ Convention/FarrisCartViewCell.m
@@ -43,7 +43,7 @@
     self.showPrice.font = discountFont;
     self.delegate = productCellDelegate;
     self.tag = tag;
-    self.min.hidden = YES; //Bill Hicks demo is using the Farris Header and we have decided to hide the Min column for now since they do not use it.
+//    self.min.hidden = YES; //Bill Hicks demo is using the Farris Header and we have decided to hide the Min column for now since they do not use it.
     self.backgroundColor = [UIColor whiteColor];//since we are using same cell for products and discounts, if a product cell is being reused, it might have a green/red background. We display discounts with white background always.
     [self updateErrorsView:[[NSSet alloc] init]];
 }
@@ -51,7 +51,8 @@
 - (void)initializeWithCart:(Cart *)cart tag:(NSInteger)tag ProductCellDelegate:(id <ProductCellDelegate>)productCellDelegate {
     self.InvtID.text = cart.product.invtid;
     [self setDescription:cart.product.descr withSubtext:cart.product.descr2];
-    self.min.text = [NilUtil objectOrDefaultString:cart.product.min defaultObject:@""];
+    NSNumber *minNumber = (NSNumber *) [NilUtil nilOrObject:cart.product.min];
+    self.min.text = minNumber ? [minNumber stringValue] : @"";
     self.quantity.text = cart.editableQty;
     self.quantity.hidden = NO;
     self.qtyLbl.hidden = YES;
@@ -59,7 +60,7 @@
     self.showPrice.text = [NumberUtil formatCentsAsCurrency:cart.product.showprc];
     self.delegate = productCellDelegate;
     self.tag = tag;
-    self.min.hidden = YES; //Bill Hicks demo is using the Farris Header and we have decided to hide the Min column for now since they do not use it.
+//    self.min.hidden = YES; //Bill Hicks demo is using the Farris Header and we have decided to hide the Min column for now since they do not use it.
     [self updateErrorsView:cart.errors];
 }
 
